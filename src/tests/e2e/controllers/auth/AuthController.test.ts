fb524937c0b5ec09f363c58d5b4aedf8e4fe7bae
samyouaret <samyouaret13@gmail.com>
2021-11-15 07:42:22 +0100

    Add authentication e2e api case
    add authenticate success to api end to end
    tests

--- src/tests/e2e/controllers/auth/AuthController.test.ts
+++ src/tests/e2e/controllers/auth/AuthController.test.ts
@@ -5,6 +5,9 @@ import { createExpressApp } from '../../../../factories/ExpressFactory';
 
 const expressApp = createExpressApp();
 const app = new Application(expressApp);
+const REGISTER_URL = '/api/auth/register';
+const LOGIN_URL = '/api/auth/login';
+
 beforeAll(async () => {
     await app.init();
 });
@@ -14,153 +17,185 @@ afterAll(async () => {
     await app.getPrisma().$disconnect();
 });
 
-it('should signup a new user', (done) => {
-    const fakeUser = {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: "Arfc1456_$1"
-    }
-    request.agent(app.getApplicationGateWay().getServer())
-        .post('/api/auth/register')
-        .set('Content-Type', 'application/json')
-        .send(fakeUser)
-        .expect(201)
-        .end(function (err, res) {
-            expect(err).toBeNull();
-            expect(res.body).toBeDefined();
-            expect(res.body).toHaveProperty('email');
-            expect(res.body).not.toHaveProperty('password');
-            app.prisma.user.findFirst({
-                where: { email: fakeUser.email }
-            }).then((user) => {
-                expect(user).toBeDefined();
-                done();
+describe('Register Routes', () => {
+    it('should signup a new user', (done) => {
+        const fakeUser = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: "Arfc1456_$1"
+        }
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(REGISTER_URL)
+            .set('Content-Type', 'application/json')
+            .send(fakeUser)
+            .expect(201)
+            .end(function (err, res) {
+                expect(err).toBeNull();
+                expect(res.body).toBeDefined();
+                expect(res.body).toHaveProperty('email');
+                expect(res.body).not.toHaveProperty('password');
+                app.prisma.user.findFirst({
+                    where: { email: fakeUser.email }
+                }).then((user) => {
+                    expect(user).toBeDefined();
+                    done();
+                });
             });
-        });
+    });
+    
+    it('should fail signup with empty fields', (done) => {
+        const user = {
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+        };
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(REGISTER_URL)
+            .set('Content-Type', 'application/json')
+            .send(user)
+            .expect(422)
+            .end(function (err, res) {
+                expect(err).toBeNull();
+                expect(Object.keys(res.body.errors).sort())
+                    .toEqual(Object.keys(user).sort());
+                app.prisma.user.findFirst({
+                    where: { email: user.email }
+                }).then((user) => {
+                    expect(user).toBeDefined();
+                    done();
+                });
+            });
+    });
+    
+    it('should fail signup with invalid email', (done) => {
+        const user = {
+            email: 'invalidemail@mail',
+            password: "Arfc1456_$1",
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+        }
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(REGISTER_URL)
+            .set('Content-Type', 'application/json')
+            .send(user)
+            .expect(422)
+            .end(function (err, res) {
+                expect(err).toBeNull();
+                expect(Object.keys(res.body.errors))
+                    .toHaveLength(1);
+                expect(res.body.errors.email.length)
+                    .toBeGreaterThan(0);
+                app.prisma.user.findFirst({
+                    where: { email: user.email }
+                }).then((user) => {
+                    expect(user).toBeDefined();
+                    done();
+                });
+            });
+    });
+    
+    it('should fail signup with short password', (done) => {
+        const user = {
+            email: faker.internet.email(),
+            password: "Fke633$",
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+        }
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(REGISTER_URL)
+            .set('Content-Type', 'application/json')
+            .send(user)
+            .expect(422)
+            .end(function (err, res) {
+                expect(err).toBeNull();
+                expect(Object.keys(res.body.errors))
+                    .toHaveLength(1);
+                expect(res.body.errors.password.length)
+                    .toBeGreaterThan(0);
+                app.prisma.user.findFirst({
+                    where: { email: user.email }
+                }).then((user) => {
+                    expect(user).toBeDefined();
+                    done();
+                });
+            });
+    });
+    
+    it('should fail signup with weak password', (done) => {
+        const user = {
+            email: faker.internet.email(),
+            password: "Far13252d",
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+        }
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(REGISTER_URL)
+            .set('Content-Type', 'application/json')
+            .send(user)
+            .expect(422)
+            .end(function (err, res) {
+                expect(err).toBeNull();
+                expect(Object.keys(res.body.errors))
+                    .toHaveLength(1);
+                expect(res.body.errors.password.length)
+                    .toBeGreaterThan(0);
+                app.prisma.user.findFirst({
+                    where: { email: user.email }
+                }).then((user) => {
+                    expect(user).toBeDefined();
+                    done();
+                });
+            });
+    });
 });
 
-it('should fail signup with empty fields', (done) => {
-    const user = {
-        email: '',
-        password: '',
-        firstName: '',
-        lastName: '',
-    };
-    request.agent(app.getApplicationGateWay().getServer())
-        .post('/api/auth/register')
-        .set('Content-Type', 'application/json')
-        .send(user)
-        .expect(422)
-        .end(function (err, res) {
-            expect(err).toBeNull();
-            expect(Object.keys(res.body.errors).sort())
-                .toEqual(Object.keys(user).sort());
-            app.prisma.user.findFirst({
-                where: { email: user.email }
-            }).then((user) => {
-                expect(user).toBeDefined();
+
+describe('Login routes', () => {
+    it('should login with valid credentials', (done) => {
+        const fakeUser = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: "Arf1756_$1"
+        }
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(REGISTER_URL)
+            .set('Content-Type', 'application/json')
+            .send(fakeUser)
+            .expect(201)
+            .end(function () {
+                let { email, password } = fakeUser;
+                request.agent(app.getApplicationGateWay().getServer())
+                    .post(LOGIN_URL)
+                    .set('Content-Type', 'application/json')
+                    .send({ email, password })
+                    .expect(200)
+                    .end(function (err, res) {
+                        expect(err).toBeNull();
+                        expect(res.body.token).toBeDefined();
+                        done();
+                    });
+            });
+    });
+
+    it('should fail login with invalid credentials', (done) => {
+        const fakeUser = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: "Arf1756_$1"
+        }
+        request.agent(app.getApplicationGateWay().getServer())
+            .post(LOGIN_URL)
+            .set('Content-Type', 'application/json')
+            .send(fakeUser)
+            .expect(401)
+            .end(function (err, res) {
+                expect(err).toBeNull();
+                expect(res.body.error).toBeDefined();
                 done();
             });
-        });
-});
-
-it('should fail signup with invalid email', (done) => {
-    const user = {
-        email: 'invalidemail@mail',
-        password: "Arfc1456_$1",
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-    }
-    request.agent(app.getApplicationGateWay().getServer())
-        .post('/api/auth/register')
-        .set('Content-Type', 'application/json')
-        .send(user)
-        .expect(422)
-        .end(function (err, res) {
-            expect(err).toBeNull();
-            expect(Object.keys(res.body.errors))
-                .toHaveLength(1);
-            expect(res.body.errors.email.length)
-                .toBeGreaterThan(0);
-            app.prisma.user.findFirst({
-                where: { email: user.email }
-            }).then((user) => {
-                expect(user).toBeDefined();
-                done();
-            });
-        });
-});
-
-it('should fail signup with short password', (done) => {
-    const user = {
-        email: faker.internet.email(),
-        password: "Fke633$",
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-    }
-    request.agent(app.getApplicationGateWay().getServer())
-        .post('/api/auth/register')
-        .set('Content-Type', 'application/json')
-        .send(user)
-        .expect(422)
-        .end(function (err, res) {
-            expect(err).toBeNull();
-            expect(Object.keys(res.body.errors))
-                .toHaveLength(1);
-            expect(res.body.errors.password.length)
-                .toBeGreaterThan(0);
-            app.prisma.user.findFirst({
-                where: { email: user.email }
-            }).then((user) => {
-                expect(user).toBeDefined();
-                done();
-            });
-        });
-});
-
-it('should fail signup with weak password', (done) => {
-    const user = {
-        email: faker.internet.email(),
-        password: "Far13252d",
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-    }
-    request.agent(app.getApplicationGateWay().getServer())
-        .post('/api/auth/register')
-        .set('Content-Type', 'application/json')
-        .send(user)
-        .expect(422)
-        .end(function (err, res) {
-            expect(err).toBeNull();
-            expect(Object.keys(res.body.errors))
-                .toHaveLength(1);
-            expect(res.body.errors.password.length)
-                .toBeGreaterThan(0);
-            app.prisma.user.findFirst({
-                where: { email: user.email }
-            }).then((user) => {
-                expect(user).toBeDefined();
-                done();
-            });
-        });
-});
-
-it('should fail login with invalid credentials', (done) => {
-    const fakeUser = {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: "Arf1756_$1"
-    }
-    request.agent(app.getApplicationGateWay().getServer())
-        .post('/api/auth/login')
-        .set('Content-Type', 'application/json')
-        .send(fakeUser)
-        .expect(401)
-        .end(function (err, res) {
-            expect(err).toBeNull();
-            expect(res.body.error).toBeDefined();
-            done();
-        });
+    });
 });
